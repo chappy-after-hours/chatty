7f772ead5d42209959530978fb1dd1d77874e809
Jeff Chapman <jeffcchapman@gmail.com>
2017-02-28 20:43:40 -0700

    finished minus black diamond

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
+
+var messages = ['Hi','Test'];
+
+app.get('/messages', function(req, res, next) {
+  res.status(200).json({messages:messages})
+});
+
+app.post('/messages', function(req, res, next) {
+  messages.push({message: req.body.message, time: new Date()});
+  res.status(200).json({messages:messages})
+})
